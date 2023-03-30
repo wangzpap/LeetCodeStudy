78e66441e101708f733d3ce5b9f54e260b085bb7
wangzhipeng <2270368003@qq.com>
2023-03-30 13:46:03 +0800

    fix conflict

--- src/main/java/leetcode/editor/cn/P1_TwoSum.java
+++ src/main/java/leetcode/editor/cn/P1_TwoSum.java
@@ -58,7 +58,7 @@ public class P1_TwoSum{
 
 	 	 //测试
 	 	 Solution s = new P1_TwoSum().new Solution();
-		  zzzz
+
 	 }
 	 
 //力扣代码
@@ -67,7 +67,10 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
 		int n = nums.length;
 		for (int i = 0; i < n; ++i) {
-
+			for (int j = i + 1; j < n; ++j) {
+				if (nums[i] + nums[j] == target) {
+					return new int[]{i, j  };
+				}
 			}
 		}
 		return new int[0];
